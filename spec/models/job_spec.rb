703cc79348447b29fa1fe1d5a3be35a600588800
Farrah Hashmi <farrahhhashmi@gmail.com>
2017-05-30 00:16:21 -0700

    updated tests

--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -26,6 +26,54 @@ context "With valid attributes" do
   expect(job).to be_invalid
   end
 
+  context "With valid attributes" do
+    it "should not save if location field is blank" do
+      job=Job.new(
+          poster:'Farrah',
+          location:'',
+          status: 'pending',
+          category: 'pets',
+          actions: 'tough job'
+      )
+    expect(job).to be_invalid
+    end
+
+  context "With valid attributes" do
+    it "should not save if Status field is blank" do
+      job=Job.new(
+          poster:'Farrah',
+          location:'New York',
+          status: '',
+          category: 'pets',
+          actions: 'tough job'
+      )
+    expect(job).to be_invalid
+    end
+
+  context "With valid attributes" do
+    it "should not save if category field is blank" do
+      job=Job.new(
+          poster:'Farrah',
+          location:'New York',
+          status: 'pending',
+          category: '',
+          actions: 'tough job'
+      )
+    expect(job).to be_invalid
+    end
+
+  context "With valid attributes" do
+    it "should not save if actions field is blank" do
+      job=Job.new(
+          poster:'Farrah',
+          location:'New York',
+          status: 'pending',
+          category: 'pets',
+          actions: ''
+      )
+    expect(job).to be_invalid
+    end
+
     it "should not save if poster is blank"
 
     it "shoud not save if location is blank"
@@ -37,4 +85,8 @@ context "With valid attributes" do
     it "should not save if actions is blank"
 
   end
+end
+end
+end
+end
 end
